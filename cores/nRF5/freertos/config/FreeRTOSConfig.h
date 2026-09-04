b3b96398a647f539f90e0087046545fd1f96ed80
Ion Popa <120658564+ion-popa@users.noreply.github.com>
2026-09-04 22:59:13 +0300

    reversed

--- cores/nRF5/freertos/config/FreeRTOSConfig.h
+++ cores/nRF5/freertos/config/FreeRTOSConfig.h
@@ -55,9 +55,7 @@
 #define configTICK_RATE_HZ                                       1024
 #define configMAX_PRIORITIES                                     ( 5 )
 #define configMINIMAL_STACK_SIZE                                 ( 100 )
-#ifndef configTOTAL_HEAP_SIZE
-	#define configTOTAL_HEAP_SIZE                                ( 4096 ) //Adjust value in platform.ini
-#endif
+#define configTOTAL_HEAP_SIZE                                    ( 4096 ) /* not used since we use malloc */
 #define configMAX_TASK_NAME_LEN                                  ( 8 )
 #define configUSE_16_BIT_TICKS                                   0
 #define configIDLE_SHOULD_YIELD                                  1
